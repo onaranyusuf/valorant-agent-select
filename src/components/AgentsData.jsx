dd38906ab4546b12584e7758d69f4539ea71714b
Yusuf <yusufonarandx@hotmail.com>
2024-05-13 20:51:24 +0300

    add agent data

---
+++ src/components/AgentsData.jsx
@@ -0,0 +1,178 @@
+import Brimstone from "../assets/agent_icons/Brimstone_icon.webp";
+import Chamber from "../assets/agent_icons/Chamber_icon.webp";
+import Viper from "../assets/agent_icons/Viper_icon.webp";
+import Raze from "../assets/agent_icons/Raze_icon.webp";
+import Omen from "../assets/agent_icons/Omen_icon.webp";
+import Sage from "../assets/agent_icons/Sage_icon.webp";
+import Skye from "../assets/agent_icons/Skye_icon.webp";
+import Killjoy from "../assets/agent_icons/Killjoy_icon.webp";
+import Cypher from "../assets/agent_icons/Cypher_icon.webp";
+import Breach from "../assets/agent_icons/Breach_icon.webp";
+import Sova from "../assets/agent_icons/Sova_icon.webp";
+import Neon from "../assets/agent_icons/Neon_icon.webp";
+import Jett from "../assets/agent_icons/Jett_icon.webp";
+import Reyna from "../assets/agent_icons/Reyna_icon.webp";
+import Phoenix from "../assets/agent_icons/Phoenix_icon.webp";
+import Yoru from "../assets/agent_icons/Yoru_icon.webp";
+import Astra from "../assets/agent_icons/Astra_icon.webp";
+import Fade from "../assets/agent_icons/Fade_icon.webp";
+import Harbor from "../assets/agent_icons/Harbor_icon.webp";
+import Gekko from "../assets/agent_icons/Gekko_icon.webp";
+import Deadlock from "../assets/agent_icons/Deadlock_icon.webp";
+import Iso from "../assets/agent_icons/Iso_icon.webp";
+import KAYO from "../assets/agent_icons/KAYO_icon.webp";
+import Clove from "../assets/agent_icons/Clove_icon.webp";
+
+import SageVideo from "../assets/agent_videos/sage_video.mp4";
+import YoruVideo from "../assets/agent_videos/yoru_video.mp4";
+
+
+
+const AgentsData = [
+  {
+    name: "Brimstone",
+    image: Brimstone,
+    video: "https://example.com/brimstone-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Chamber",
+    image: Chamber,
+    video: "https://example.com/chamber-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Viper",
+    image: Viper,
+    video: "https://example.com/viper-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Raze",
+    image: Raze,
+    video: "https://example.com/raze-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Omen",
+    image: Omen,
+    video: "https://example.com/omen-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Sage",
+    image: Sage,
+    video: SageVideo,
+    selected: false,
+  },
+  {
+    name: "Skye",
+    image: Skye,
+    video: "https://example.com/skye-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Killjoy",
+    image: Killjoy,
+    video: "https://example.com/killjoy-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Cypher",
+    image: Cypher,
+    video: "https://example.com/cypher-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Breach",
+    image: Breach,
+    video: "https://example.com/breach-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Sova",
+    image: Sova,
+    video: "https://example.com/sova-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Neon",
+    image: Neon,
+    video: "https://example.com/neon-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "KAY/O",
+    image: KAYO,
+    video: "https://example.com/kayo-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Jett",
+    image: Jett,
+    video: "https://example.com/jett-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Reyna",
+    image: Reyna,
+    video: "https://example.com/reyna-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Phoenix",
+    image: Phoenix,
+    video: "https://example.com/phoenix-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Clove",
+    image: Clove,
+    video: "https://example.com/clove-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Yoru",
+    image: Yoru,
+    video: YoruVideo,
+    selected: false,
+  },
+  {
+    name: "Astra",
+    image: Astra,
+    video: "https://example.com/astra-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Fade",
+    image: Fade,
+    video: "https://example.com/fade-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Harbor",
+    image: Harbor,
+    video: "https://example.com/harbor-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Gekko",
+    image: Gekko,
+    video: "https://example.com/gekko-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Deadlock",
+    image: Deadlock,
+    video: "https://example.com/deadlock-intro.mp4",
+    selected: false,
+  },
+  {
+    name: "Iso",
+    image: Iso,
+    video: "https://example.com/iso-intro.mp4",
+    selected: false,
+  },
+];
+
+export default AgentsData;
